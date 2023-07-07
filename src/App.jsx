5be6d028d9ad8d3f4251cbbe5a5dbe9e8fe86818
emanuel heredia <ema_359@hotmail.com>
2023-07-07 17:49:35 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -49,6 +49,26 @@ function App() {
 		let columnIndexPrice = null;
 		let columnIndexMoreInfo = null;
 		let columnIndexMark = null;
+		let nullElemnt = 0;
+		let isTableHeader = false;
+		for (let el of row) {
+			if (el === null) {
+				nullElemnt += 1;
+			}
+			if (tableHeaders.includes(el)) {
+				isTableHeader = true;
+			}
+		}
+		if (
+			nullElemnt === row.length ||
+			nullElemnt === row.length - 1 ||
+			isTableHeader
+		) {
+			return true;
+		}
+		return false;
+	};
+
 	};
 	return (
 		<>
