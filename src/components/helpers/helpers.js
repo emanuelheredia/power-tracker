12b103e4455f8d0e1531393f82018d7426850d67
emanuel heredia <ema_359@hotmail.com>
2023-09-06 15:49:53 -0300

    probando despliegue con servidor

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,5 +1,6 @@
 export const structuringSelectValues = (array) => {
-	let allValuesStructured = array.map((el) => {
+	let orderArray = [...array];
+	let allValuesStructured = orderArray.sort().map((el) => {
 		return { label: el.toUpperCase(), value: el };
 	});
 	allValuesStructured.unshift({
